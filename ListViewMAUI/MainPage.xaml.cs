a6ca93759145e0d5a71fc72661a086e2ad218f61
RiyashameedM <riyashameed.m@syncfusion.com>
2025-06-20 16:14:14 +0530

    Added the sample into the repository

---
+++ ListViewMAUI/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ListViewMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ ListViewMAUI/ViewModel/ViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewMAUI
+{
+	public class ViewModel : INotifyPropertyChanged
+	{
+		#region Fields
+
+		private dynamic itemsSource;
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		#endregion
+
+		#region Properties
+		public dynamic ItemsSource
+		{
+			get
+			{
+				return itemsSource;
+			}
+
+			set
+			{
+				itemsSource = value;
+				RaisepropertyChanged("ItemsSource");
+			}
+		}
+
+		private void RaisepropertyChanged(string v)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(v));
+		}
+		#endregion
+
+		#region Constructor
+		public ViewModel()
+		{
+			var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+			Stream stream = assembly.GetManifestResourceStream("ListViewMAUI.Data.Data.json");
+			using (StreamReader sr = new StreamReader(stream))
+			{
+				var jsonText = sr.ReadToEnd();
+				ItemsSource = JsonConvert.DeserializeObject<dynamic>(jsonText);
+			}
+
+		}
+		#endregion
+
+	}
+}
